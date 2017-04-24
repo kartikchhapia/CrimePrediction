b445e49105fe16339d4f4904f3bcfc7f65d2fde6
Ajinkya Indulkar <ajinkya.anant.indulkar@gmail.com>
2017-04-24 19:45:20 -0400

    Use parallel requests to get census data

--- code/CensusDataExtract/getCensusCSV.py
+++ code/CensusDataExtract/getCensusCSV.py
@@ -1,34 +1,41 @@
-import numpy as np
-import csv
+import time
+import pandas as pd
 import extract_data
-import cPickle as p
-from numpy import genfromtxt
+from config import THROTTLE
+from tqdm import tqdm
 
-def get_census_csv(pointList):
+
+def get_census_csv(locations):
     """
     Iterates over all datapoints to get census data
     Takes dictionary census data for every row and stores as an array
     saves as csv
     """
-    count = 0
-    dictList = []
-        
-    for i in pointList :
-        xDict = extract_data.get_census_data(i[0],i[1],year='2010')
-        dictList.append(xDict)
-        count += 1
-        print 'num datapoints read:'+ str(count)
-
-    valList = map(lambda x : x.values(), dictList)
-    valList = np.array(valList)
-    with open('censusList0.csv', 'wb') as f:
-        writer = csv.writer(f)
-        writer.writerows(valList)
-    print 'writing done'
-    return valList
+    census_dict_list = []
+    chunk_size = 8
+    it = iter(locations)
+    with tqdm(total=len(locations)) as pbar:
+        for lat, lon in it:
+            time.sleep(THROTTLE)
+            i = 0
+            coords = [(lat, lon)]
+            try:
+                while i < chunk_size - 1:
+                    coords.append(next(it))
+                    i += 1
+            except StopIteration:
+                print 'No more elements'
+            census_dicts = extract_data.get_census_data_async(coords)
+            census_dict_list.extend(census_dicts)
+            pbar.update(chunk_size)
+        df_cen = pd.DataFrame.from_dict(census_dict_list)
+        return df_cen
 
 
 if __name__ == '__main__':
-    myData = genfromtxt('../../data/TestDataSet/crimedata.csv', delimiter=',')
-    locationsInNYC = myData[1:530,1:3] #531 : all
-    data = get_census_csv(locationsInNYC)
+    df = pd.read_csv('../../data/TestDataSet/crime_landmarks.csv', index_col=0)
+    # locs = myData[1:530, 1:3] #531 : all
+    locations = df[['Lat', 'Lon']].values.tolist()
+    df_cen = get_census_csv(locations)
+    df_res = pd.concat([df, df_cen], axis=1)
+    df_res.to_csv('../../data/TestDataSet/crime_landmarks_census.csv')
